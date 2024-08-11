ced80971e627c6473a182827f11f4a210f7dfe2f
sieun07 <lse0707@sangilmedia.hs.kr>
2024-08-11 12:56:00 +0900

    20240811

--- practice/test.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-def current_time():
-    now = datetime.now()
-    time_string = now.strftime("%Y년 %m월 %d %H시 %M분 %S초")
-    return(f"현재시간: {time_string}")
-
-if __name__ == '__main__':
-    print(current_time())
